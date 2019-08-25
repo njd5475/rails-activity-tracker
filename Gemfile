8f7cc1551be0bba3cdf3b418dbf3a70b8f6101e3
nick <njd5475@gmail.com>
2019-08-24 23:42:48 -0400

    Upgrade to rails 6.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 # add additional items here at the top.
 ruby "2.5.1"
-gem 'bundler', '>= 1.8.4'
 
 gem 'exception_notification'
 
@@ -25,10 +24,10 @@ gem 'bower-rails', '~> 0.9.2'
 gem 'jquery-turbolinks'
 gem 'backbone-rails'
 gem 'haml-rails'
-gem 'react-rails', '= 2.0.0'
+gem 'react-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.11'
+gem 'rails', '~> 6.0'
 # Use postgres as the database for Active Record
 gem 'pg', '~> 0.20'
 # Use SCSS for stylesheets
@@ -36,7 +35,7 @@ gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
+gem 'coffee-rails', '~> 5.0.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
@@ -44,10 +43,6 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.0'
-# bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -60,6 +55,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use passenger for development
 gem 'passenger', group: :development
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -76,4 +72,6 @@ group :development, :test do
   gem 'sqlite3'
 
   gem 'pry'
+
+  gem 'listen'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,66 +1,62 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190415152156) do
+ActiveRecord::Schema.define(version: 2019_08_25_033943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activities", force: :cascade do |t|
-    t.string   "description"
+  create_table "activities", id: :serial, force: :cascade do |t|
+    t.string "description"
     t.datetime "start"
     t.datetime "end"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
-  add_index "activities", ["user_id"], name: "index_activities_on_user_id", using: :btree
-
-  create_table "goals", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "activities_id"
-    t.string   "name"
-    t.date     "target_date"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.string   "target_value"
-    t.string   "default_value"
-    t.string   "current_value"
+  create_table "goals", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "activities_id"
+    t.string "name"
+    t.date "target_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "target_value"
+    t.string "default_value"
+    t.string "current_value"
+    t.index ["activities_id"], name: "index_goals_on_activities_id"
+    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
-  add_index "goals", ["activities_id"], name: "index_goals_on_activities_id", using: :btree
-  add_index "goals", ["user_id"], name: "index_goals_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "provider"
-    t.string   "uid"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "activities", "users"
 end
